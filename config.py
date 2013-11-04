cbb7b0ee3aaf917c9aa9b3e4fa8e950810eee51f
mylsb <personal.mitchellstewart@gmail.com>
2013-11-04 14:28:55 +0000

    Local launch config and style changes

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+BASE_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
+
 CSRF_ENABLED = True
 SECRET_KEY = '!th1553cr3tk3yha5thaty0l0swag!'
+
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIRECTORY, 'app.db') + '?check_same_thread=False'
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
