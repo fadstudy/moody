3c09cbdcec217cc2b69aeb4d91c3f6ac7104db4c
mylsb <personal.mitchellstewart@gmail.com>
2014-03-23 13:16:24 +0000

    Added retrospective and consent form models

--- app/forms.py
+++ app/forms.py
@@ -22,6 +22,43 @@ class AdvancedMoodForm(Form):
                  choices=[('0', 'No'), ('1', 'Yes')], validators=[Required()])
     hospital_reason = BooleanField('Was it related to this condition?')
 
-    medication = RadioField('Has your medication changed in the last two weeks?',
+    medication = RadioField('Has your medication changed in the last two \
+                            weeks?',
                  choices=[('0', 'No'), ('1', 'Yes')], validators=[Required()])
     medication_reason = BooleanField('Was it related to this condition?')
+
+
+class ConsentToStudyForm(Form):
+    project_consent = BooleanField('I consent to this research project as \
+                                   part of an understanding that the times I \
+                                   use facebook will be analysed by MAPRC and \
+                                   RMIT researchers and that this project \
+                                   will ask me to rate my mood.',
+                                   validators=[Required()])
+    age_consent = BooleanField('I agree that I am between the ages of 18-30, \
+                               am informed about this study\'s intent and \
+                               general nature - including any possible risks \
+                               of involvement - and that I am consenting for \
+                               myself only and on no-ones behalf.',
+                               validators=[Required()])
+    diagnosed_consent = BooleanField('I acknowledge that I have a prior \
+                                     diagnosis of bipolar disorder by an \
+                                     Australian Doctor.',
+                                     validators=[Required()])
+
+
+class Episode(Form):
+    date_of_episode = TextField('Date of episode', validators=[Required()])
+    nature_of_episode = RadioField('Nature of episode', choices=[('0', 'Mania'),
+                                   ('1', 'Depression')],
+                                   validators=[Required()])
+    hospitalisation = RadioField('Did this result in hospitalisation?',
+                                 choices=[('0', 'No'), ('1', 'Yes')],
+                                 validators=[Required()])
+    medication_change = RadioField('Did this result in change to your \
+                                   medication?',
+                                   choices=[('0', 'No'), ('1', 'Yes')],
+                                   validators=[Required()])
+    suicidal_ideation = RadioField('Did you experience suicidal ideation?',
+                                   choices=[('0', 'No'), ('1', 'Yes')],
+                                   validators=[Required()])
